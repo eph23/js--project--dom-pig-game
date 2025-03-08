b3831d1cd76e3b51ef70bca1e9589b319f49d854
ephraim <ephraims.818@gmail.com>
2025-03-08 21:30:37 +0600

    JS--DOM--PIG-GAME - 03. Switchig the Active Player

---
+++ 03. Switching the Active Player/script.js
@@ -0,0 +1,47 @@
+'use strict';
+
+// Selecting elements
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
+
+const score0El = document.querySelector('#score--0');
+const score1El = document.querySelector('#score--1');
+const current0El = document.querySelector('#current--0');
+const current1El = document.querySelector('#current--1');
+
+const diceEl = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+
+// Initial condition
+score0El.textContent = 0;
+score1El.textContent = 0;
+diceEl.classList.add('hidden');
+
+const scores = [0, 0];
+let currentScore = 0;
+let activePlayer = 0;
+
+// Dice roll functionality
+btnRoll.addEventListener('click', function () {
+  const dice = Math.trunc(Math.random() * 6) + 1;
+  console.log(dice);
+  diceEl.classList.remove('hidden');
+  diceEl.src = `dice-${dice}.png`;
+
+  if (dice !== 1) {
+    currentScore += dice;
+    document.querySelector(`#current--${activePlayer}`).textContent =
+      currentScore;
+  }
+  //   Switch player
+  else {
+    currentScore = 0;
+    document.querySelector(`#current--${activePlayer}`).textContent = 0;
+
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    player0El.classList.toggle('player--active');
+    player1El.classList.toggle('player--active');
+  }
+});
