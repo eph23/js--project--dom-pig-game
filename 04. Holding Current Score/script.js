a3e23099d497b6cda893e36d52cac15999d36eff
ephraim <ephraims.818@gmail.com>
2025-03-09 22:20:08 +0600

    JS--DOM--PIG-GAME - 04. Holding Current Score

--- 04. Holding Current Score/script.js
+++ 04. Holding Current Score/script.js
@@ -52,3 +52,24 @@ btnRoll.addEventListener('click', function () {
     }
   }
 });
+
+btnHold.addEventListener('click', function () {
+  if (playing) {
+    scores[activePlayer] += currentScore;
+    document.querySelector(`#score--${activePlayer}`).textContent =
+      scores[activePlayer];
+
+    if (scores[activePlayer] >= 20) {
+      playing = false;
+      diceEl.classList.add('hidden');
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.add('player--winner');
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.add('player--active');
+    } else {
+      switchPlayer();
+    }
+  }
+});
